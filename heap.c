e0aac06f90e8e881d01875aac0af41a4c78bf507
Replit user <>
2023-05-12 18:55:14 +0000

    new try

--- heap.c
+++ heap.c
@@ -1,21 +1,20 @@
+#include "heap.h"
+#include <ctype.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
-#include <ctype.h>
-#include "heap.h"
 
-typedef struct nodo{
-   void* data;
-   int priority;
-}heapElem;
+typedef struct nodo {
+  void *data;
+  int priority;
+} heapElem;
 
-typedef struct Heap{
-  heapElem* heapArray;
+typedef struct Heap {
+  heapElem *heapArray;
   int size;
   int capac;
 } Heap;
-
 
 void *heap_top(Heap *pq) {
   if (pq == NULL || pq->size == 0)
@@ -23,29 +22,29 @@ void *heap_top(Heap *pq) {
   return pq->heapArray[0].data;
 }
 
-
-
 void heap_push(Heap *pq, void *data, int priority) {
-  //check 0
+  // check 0
   if (pq == NULL || pq->size == 0)
     return;
-  //check lleno
-  if (pq->size == pq->capac) 
-  {
+
+  if (pq->size == pq->capac) {
     int ncap = (2 * pq->capac) + 1;
     pq->heapArray = realloc(pq->heapArray, ncap * sizeof(heapElem));
     pq->capac = ncap;
   }
+  int i = pq->size;
+  pq->heapArray[i].data = data;
+  pq->heapArray[i].priority = priority;
 
+  while (i > 0 && pq->heapArray[i].priority > pq->heapArray[(i - 1) / 2].priority) {
+    heapElem vtemp = pq->heapArray[i];
+    pq->heapArray[i] = pq->heapArray[(i - 1) / 2];
+    pq->heapArray[(i - 1) / 2] = vtemp;
+    i = (i - 1) / 2;
+  }
+  pq->size++;
 }
 
+void heap_pop(Heap *pq) {}
 
-
-void heap_pop(Heap* pq){
-
-}
-
-Heap* createHeap(){
-
-   return NULL;
-}
+Heap *createHeap() { return NULL; }
